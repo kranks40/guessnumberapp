c6bd8e7141d207a1b430c16981a65c94e2a37aa9
kranks40 <eng_peters20@yahoo.com>
2020-11-25 18:38:36 -0400

    Made some changes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,28 +1,65 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
 import Card from '../components/Card';
 
 const StartGameScreen = (props) => {
+    const [ enteredValue, setEnteredValue ] = useState('');
+    const [ confirmed, setConfirmed ] = useState(false);
+    const [ selectedNumber, setSelectedNumber ] = useState();
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    };
+
+    const resetInputHandler = () => {
+        setEnteredValue('')
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chooseNumber = parseInt(enteredValue)
+        if ( isNaN(chooseNumber) || chooseNumber <= 0 || chooseNumber > 99)
+        Alert.alert('Invalid input', 
+        'Please enter values between 1 - 99', 
+        [{text: 'Okay', style: "destructive", onPress: resetInputHandler }]
+        )
+        return;
+    
+
+        setConfirmed(true);
+        setEnteredValue('');
+        setSelectedNumber(chooseNumber);
+    };
+
+
     return (
-        <View style={styles.startScreen}>
-            <Text style={styles.startScreen__title}>Start a new Game</Text>
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+            <View style={styles.startScreen}>
+                <Text style={styles.startScreen__title}>Start a new Game</Text>
 
-            <Card style={styles.startScreen__input}>
-                <Text>Select a Number</Text>
-                <TextInput {...props} style={styles.startScreen__textInput} keyboardType='numeric' maxLength={2} selectTextOnFocus={true} />
+                <Card style={styles.startScreen__input}>
+                    <Text>Select a Number</Text>
+                    <TextInput {...props}
+                    style={styles.startScreen__textInput}
+                    keyboardType='numeric' maxLength={2} 
+                    selectTextOnFocus={true} 
+                    onChangeText={numberInputHandler}
+                    value={enteredValue}
+                    />
 
-                <View style={styles.startScreen__button}>
-                    <View style={styles.reset__button}>
-                    <Button title='Reset' color='#b12704' onPress={() => {}} />
+                    <View style={styles.startScreen__button}>
+                        <View style={styles.reset__button}>
+                        <Button title='Reset' color='#b12704' onPress={resetInputHandler} />
+                        </View>
+                        <View style={styles.confirm__button}>
+                        <Button title='Confirm' color='#f2ae59' onPress={confirmInputHandler} />
+                        </View>
+                        
+                        
                     </View>
-                    <View style={styles.confirm__button}>
-                    <Button title='Confirm' color='#f2ae59' onPress={() => {}} />
-                    </View>
-                    
-                    
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
